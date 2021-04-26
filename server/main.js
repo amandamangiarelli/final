f2df5adeb16cfd9aa5e6bdad3782e22fc443aa89
amandamangiarelli <46465525+amandamangiarelli@users.noreply.github.com>
2021-04-26 00:49:32 -0400

    updated mainjs

--- server/main.js
+++ server/main.js
@@ -1,5 +1,14 @@
 import { Meteor } from 'meteor/meteor';
+import { UP_Collection_Access } from './../imports/api/user_posts.js';
+import { UC_Collection_Access } from './../imports/api/user_comments.js';
 
-Meteor.startup(() => {
-  // code to run on server at startup
+//Meteor.startup(() => {
+Meteor.startup(function() {
+  UC_Collection_Access.insert ({
+    name: 'person',
+    comment: 'comment test',
+  });
+
+console.log(UC_Collection_Access.find().fetch());
+
 });
