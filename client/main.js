b6d581e1a27cb35e8a5c1d2a84650da0d41965f4
amandamangiarelli <46465525+amandamangiarelli@users.noreply.github.com>
2021-04-26 00:49:04 -0400

    client files

--- client/main.js
+++ client/main.js
@@ -1,22 +1,32 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Meteor} from 'meteor/meteor';
+import {UP_Collection_Access, Calculate_rank_and_position_for_posts} from './../imports/api/user_posts.js';
+import {UC_Collection_Access, Calculate_rank_and_position_for_comments} from './../imports/api/user_comments.js';
+import App from './../imports/ui/App.js';
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
+Meteor.startup(() =>  {
+  Tracker.autorun(() => {
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
+    let allPostsInDB = UP_Collection_Access.find({/*empty so get all posts */},
+                                                  {sort: {votes: -1}}).fetch();
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+    let allCommentsInDB = UC_Collection_Access.find({/*empty so get all comments */},
+                                                {sort: {votes: -1}}).fetch();
+
+    let title = 'Facebook';
+    let positioned_posts = Calculate_rank_and_position_for_posts(allPostsInDB);
+    let positioned_comments = Calculate_rank_and_position_for_comments(allCommentsInDB);
+
+    ReactDOM.render(<App
+        passedPropTitle={title}
+        passedPropCreator={'Creator: Amanda'}
+        passedPropAllPosts={allPostsInDB}
+        passedPropAllPosts={positioned_posts}
+        passedPropAllComments={allCommentsInDB}
+        passedPropAllComments={positioned_comments}
+        passedFooter={'\u00A9 Amanda Mangiarelli 2021'}
+      />, document.getElementById('content'));
+  });
 });
